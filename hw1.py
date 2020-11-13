ceeadf2a0e789d2e408453fa06f1308f2ce0faaa
cheng cet <71220777+immortalsh@users.noreply.github.com>
2020-11-14 00:24:40 +0800

    python 2nd homework

---
+++ hw1.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 13 19:54:56 2020
+
+@author: Lenovo
+"""
+
+motorcycles=['apple','banana','cat']
+print(motorcycles[0])
+motorcycles.append('dog')
+print(motorcycles) 
+motorcycles.insert(1, 'im')
+print(motorcycles)
+del motorcycles[0]
+print(motorcycles)
+print(sorted(motorcycles))
+print(motorcycles)
+motorcycles.reverse()
+print(motorcycles)
+print(len(motorcycles))
+print(motorcycles[-1])
+for motorcycle in motorcycles:
+    print(motorcycle)
+
+
+
+      
+
+
