f0481c922b81d603334aa2916125d2ceb132f8da
cheng cet <71220777+immortalsh@users.noreply.github.com>
2020-11-14 00:55:29 +0800

    'if'grammar training

---
+++ if-training.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 14 00:41:08 2020
+
+@author: Lenovo
+"""
+
+alien_colors=['green','red','yellow']
+for alien_color in alien_colors:
+    if alien_color=='green':
+        print('Success,you got 5 points')
+    elif alien_color=='red':
+        print('you failed')
+    elif alien_color=='yellow':
+        print('you failed')
