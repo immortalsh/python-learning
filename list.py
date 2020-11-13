5652b017dfde3d31eda9fb72bcb1c9aa89812a5c
cheng cet <71220777+immortalsh@users.noreply.github.com>
2020-11-14 00:27:59 +0800

    2 homework

---
+++ list.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 13 23:49:19 2020
+
+@author: Lenovo
+"""
+
+for value in range(1,5):
+    print(value)
+number=list(range(1,5))
+print(number)
+numbers=list(range(2,11,2))
+print(numbers)
+    
