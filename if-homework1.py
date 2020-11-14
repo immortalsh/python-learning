e11bde13f814cd5e418f56ec4613b1387f881b10
cheng cet <71220777+immortalsh@users.noreply.github.com>
2020-11-14 15:49:25 +0800

    train the use of 'if'

---
+++ if-homework1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 14 15:45:35 2020
+
+@author: Lenovo
+"""
+
+users=['admin','cet','johnson','alice','immortalsh']
+for user in users:
+    if user=='admin':
+        print('Hello admin, would you like to see a status report?')
+    else:
+        print('Hello Eric, thank you for logging in again”')
