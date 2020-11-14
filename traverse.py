81f6797bf0c1247ff42daf7acbcb5f2aa084a654
cheng cet <71220777+immortalsh@users.noreply.github.com>
2020-11-14 16:37:19 +0800

    learn how to traverse dict

---
+++ traverse.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 14 16:16:48 2020
+
+@author: Lenovo
+"""
+#traverse.py
+user_0 = {
+'username': 'efermi',
+'first': 'enrico',
+'last': 'fermi',
+}
+for key,value in user_0.items():
+    print("\nKey: "+key)
+    print("\nvalue:"+value)
+favorite_languages = {
+'jen': 'python',
+'sarah': 'c',
+'edward': 'ruby',
+'phil': 'python',
+}
+for name in favorite_languages.keys():
+    print("\n"+name.title())
