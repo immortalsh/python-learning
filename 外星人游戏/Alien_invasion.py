f5abd202c2eef075157853994bb6b798c0eb5af9
immortalsh <sh603721@sjtu.edu.cn>
2021-03-08 18:38:53 +0800

    3/8修改配置

--- 外星人游戏/Alien_invasion.py
+++ 外星人游戏/Alien_invasion.py
@@ -12,26 +12,26 @@ from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
 
+
 def run_game():
     pygame.init()
-    ai_settings=Settings()
-    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height)
+    )
     pygame.display.set_caption("Alien invasion")
-    ship=Ship(ai_settings,screen)
-    #创建一个子弹编组
-    bullets=Group()
-    aliens=Group()
-    #创建一行外星人
-    gf.create_fleet(ai_settings,screen,aliens)
-    while True:#开始主循环
-          gf.check_events(ai_settings,screen,ship,bullets)
-          ship.update()
-          gf.update_bullets(bullets)
-          gf.update_screen(ai_settings,screen,ship,aliens,bullets)
-          gf.update_aliens(aliens)
+    ship = Ship(ai_settings, screen)
+    # 创建一个子弹编组
+    bullets = Group()
+    aliens = Group()
+    # 创建一行外星人
+    gf.create_fleet(ai_settings, screen, aliens)
+    while True:  # 开始主循环
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_aliens(aliens)
+
+
 run_game()
-   
-
-
-    
-    
--- 外星人游戏/settings.py
+++ 外星人游戏/settings.py
@@ -5,18 +5,21 @@ Created on Sat Dec 12 14:45:00 2020
 @author: Lenovo
 """
 
-#settings.py
-class Settings():
+# settings.py
+class Settings:
     def __init__(self):
-        
+
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230,230,230)
-        self.ship_speed_factor=1.5
-        #子弹
-        self.bullet_speed_factor=1
-        self.bullet_width=3
-        self.bullet_height=15
-        self.bullet_color=(60,60,60)
-        self.bullet_allowed=3
-        self.alien_speed_factor=1
+        self.bg_color = (230, 230, 230)
+        self.ship_speed_factor = 1.5
+        # 子弹
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullet_allowed = 3
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # 1为向右移，-1为向左移
+        self.fleet_direction = 1
